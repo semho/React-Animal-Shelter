3be1665dd83f4c71d4bac188cf8259d0e64cdd9f
unknown <semho@yandex.ru>
2022-06-19 17:18:46 +0300

    filled dropdown items

--- src/shared/CardsList/Card/Menu/Menu.tsx
+++ src/shared/CardsList/Card/Menu/Menu.tsx
@@ -1,31 +1,28 @@
 import React from 'react';
-import { generateId, generateRandomString } from '../../../../utils/react/generateRandomIndex';
-import { GenericList } from './GenericList';
-import { merge } from '../../../../utils/js/merge';
+import { MenuIcon } from '../../../Icons/MenuIcon';
 import styles from './menu.css';
-import { Dropdown } from './Dropdown';
-
-const LIST = [
-  { As: 'li' as const, text: 'first' },
-  { As: 'li' as const, text: 'second' },
-  { As: 'li' as const, text: 'third' },
-].map(generateId);
+import { Dropdown } from '../../../Dropdown';
+import { EColor, Text } from '../../../Text'; 
+import { MenuItemsList } from './MenuItemsList';
 
 export function Menu() {
   return (
     <div className={styles.menu}>
-      <Dropdown button={
-        <button className={styles.menuButton}>
-          <svg width="5" height="20" viewBox="0 0 5 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="2.5" cy="2.5" r="2.5" fill="#D9D9D9"/>
-            <circle cx="2.5" cy="10" r="2.5" fill="#D9D9D9"/>
-            <circle cx="2.5" cy="17.5" r="2.5" fill="#D9D9D9"/>
-          </svg>
-        </button>
-      }>
-        <ul>
-          <GenericList list={LIST} />
-        </ul>
+      <Dropdown 
+        button={
+          <button className={styles.menuButton}>
+            <MenuIcon />
+          </button>
+        }
+      >
+        <div className={styles.dropdown}>
+          <MenuItemsList postId='test'/>
+          <button className={styles.closeButton}>
+            <Text mobileSize={12} size={14} color={EColor.grey66}>
+              Закрыть
+            </Text>
+          </button>
+        </div>
       </Dropdown>
 
     </div>
---
+++ src/shared/Icons/Index.ts
@@ -0,0 +1,7 @@
+export * from './MenuIcon';
+export * from './BlockIcon';
+export * from './WarningIcon';
+export * from './CommentsIcon';
+export * from './ShareIcon';
+export * from './SaveIcon';
+export * from './Icon';
